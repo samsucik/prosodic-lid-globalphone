a2e079c2fdf305a6a740a567f95a901b7aed4dc6
Sam Sucik <sucik.samo@gmail.com>
2019-02-19 11:34:53 +0000

    Changed xvector-fusion code so that it takes feature types and other arguments from the command line.

--- gp-xvectors-recipe/run_feature_fusion.sh
+++ gp-xvectors-recipe/run_feature_fusion.sh
@@ -1,30 +1,56 @@
 #!/bin/bash -u
 
-source path.sh
+feat1=mfcc
+feat2=pitch
+use_test_set=false
+
+if [ -f path.sh ]; then source ./path.sh; fi
+source parse_options.sh || exit 1;
+
+if [ $# -lt 2 ]; then
+   echo "Usage: $0 [options] <feat1> <feat2>";
+   echo "e.g.: $0 mfcc pitch_energy"
+   echo " Options:"
+   echo "  --use-test-set <bool|false>  # Whether to do scoring on the test set as well (in addition to eval set)"
+   exit 1;
+else
+  feat1=$1
+  feat2=$2
+fi
+log_prefix="\n$0"
+
+echo -e "${log_prefix}Doing fusion of ${feat1} and ${feat2}."
 
 GP_LANGUAGES="AR BG CH CR CZ FR GE JA KO PL PO RU SP SW TA TH TU WU VN"
-exp_dir=exp/xvectors_fusion
+exp_dir=exp/fusion_${feat1}_${feat2}
 classifier_dir=$exp_dir/classifier
+exp_base_dir=~/lid
 
-exp1=~/lid/mfcc
-exp2=~/lid/pitch_energy
+echo -e "${log_prefix}Storing everything in ${exp_dir}."
+
+feat1_dir=$exp_base_dir/$feat1
+feat2_dir=$exp_base_dir/$feat2
 
 mkdir -p exp
 mkdir -p $exp_dir
 mkdir -p $exp_dir/results
 mkdir -p $classifier_dir
 
-for dataset in enroll eval; do
-  echo "Concatenating ${dataset} xvectors..."
+if [ "$use_test_set" = true ]; then
+  $datasets="enroll eval test"
+else
+  $datasets="enroll eval"
+fi
+
+for dataset in $datasets; do
+  echo -e "${log_prefix}Concatenating ${dataset} xvectors..."
   
   mkdir -p $exp_dir/xvectors_${dataset}
-  cp $exp1/exp/xvectors_${dataset}/utt2lang $exp_dir/xvectors_${dataset}
-  xvectors1=$exp1/exp/xvectors_${dataset}/xvector.scp
-  xvectors2=$exp2/exp/xvectors_${dataset}/xvector.scp
+  cp $feat1_dir/exp/xvectors_${dataset}/utt2lang $exp_dir/xvectors_${dataset}
+  xvectors1=$feat1_dir/exp/xvectors_${dataset}/xvector.scp
+  xvectors2=$feat2_dir/exp/xvectors_${dataset}/xvector.scp
 
-  echo "$xvectors1"
-  echo "$xvectors2"
-
+  (
   paste-feats \
     ark:"copy-vector scp:$xvectors1 ark,t:- | sed -E 's/\[/\[\n/' | sed -E 's/\]/\n\]/' |" \
     ark:"copy-vector scp:$xvectors2 ark,t:- | sed -E 's/\[/\[\n/' | sed -E 's/\]/\n\]/' |" \
@@ -34,7 +60,9 @@ for dataset in enroll eval; do
 
   copy-vector ark,t:$exp_dir/xvectors_${dataset}/xvector_tmp.ark \
     ark,scp:$exp_dir/xvectors_${dataset}/xvector.ark,$exp_dir/xvectors_${dataset}/xvector.scp
+  ) > $exp_dir/concat_xvectors_${dataset}.log
 done
+
 
 langs=($GP_LANGUAGES)
 i=0
@@ -43,7 +71,7 @@ for l in "${langs[@]}"; do
   i=$(expr $i + 1)
 done > conf/test_languages.list
 
-# Training the log reg model
+echo -e "${log_prefix} Training the log reg model..."
 ./local/logistic_regression_train.sh \
   --prior-scale 0.70 \
   --conf conf/logistic-regression.conf \
@@ -53,19 +81,29 @@ done > conf/test_languages.list
   --eval-utt2lang $exp_dir/xvectors_eval/utt2lang \
   --languages conf/test_languages.list \
   &> $exp_dir/classifier/logistic-regression-train.log
-  
-# Classifying eval set samples
-./local/logistic_regression_score.sh \
-  --languages conf/test_languages.list \
-  --model-dir $classifier_dir \
-  --test-dir $exp_dir/xvectors_eval \
-  --classification-file $exp_dir/results/classification-eval \
-  &> $exp_dir/classifier/logistic-regression-score-eval.log
 
-# Computing results
-./local/compute_results.py \
-  --classification-file $exp_dir/results/classification-eval \
-  --output-file $exp_dir/results/results-eval \
-  --conf-mtrx-file $exp_dir/results/conf_matrix-eval.csv \
-  --language-list "$GP_LANGUAGES" \
-  &>$exp_dir/results/compute_results-eval.log
+
+
+if [ "$use_test_set" = true ]; then
+  $datasets="eval test"
+else
+  $datasets="eval"
+fi
+
+for dataset in $datasets; do
+  echo -e "${log_prefix}Scoring eval set samples..."
+  ./local/logistic_regression_score.sh \
+    --languages conf/test_languages.list \
+    --model-dir $classifier_dir \
+    --test-dir $exp_dir/xvectors_${dataset} \
+    --classification-file $exp_dir/results/classification-${dataset} \
+    &> $exp_dir/classifier/logistic-regression-score-${dataset}.log
+
+  echo -e "${log_prefix}Computing results..."
+  ./local/compute_results.py \
+    --classification-file $exp_dir/results/classification-${dataset} \
+    --output-file $exp_dir/results/results-${dataset} \
+    --conf-mtrx-file $exp_dir/results/conf_matrix-${dataset}.csv \
+    --language-list "$GP_LANGUAGES" \
+    &>$exp_dir/results/compute_results-${dataset}.log
+fi
