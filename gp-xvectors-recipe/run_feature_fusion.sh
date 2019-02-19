c683792abd06844bcce8d334283009142f0ba64a
samsucik <sucik.samo@gmail.com>
2019-02-19 11:11:03 +0000

    Debugged the xvector concatenation code. It works! And even outperforms the feature concatenation setup.

--- gp-xvectors-recipe/run_feature_fusion.sh
+++ gp-xvectors-recipe/run_feature_fusion.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -u
 
+source path.sh
+
 GP_LANGUAGES="AR BG CH CR CZ FR GE JA KO PL PO RU SP SW TA TH TU WU VN"
-exp_dir=exp/xvector_fusion
+exp_dir=exp/xvectors_fusion
 classifier_dir=$exp_dir/classifier
 
 exp1=~/lid/mfcc
 exp2=~/lid/pitch_energy
 
+mkdir -p exp
 mkdir -p $exp_dir
 mkdir -p $exp_dir/results
 mkdir -p $classifier_dir
@@ -14,15 +17,23 @@ mkdir -p $classifier_dir
 for dataset in enroll eval; do
   echo "Concatenating ${dataset} xvectors..."
   
-  mkdir -p $exp_dir/xvector_${dataset}
-  cp $exp1/exp/xvector_${dataset}/utt2lang $exp_dir/xvector_${dataset}
-  xvectors1=$exp1/exp/xvector_${dataset}/xvector.scp
-  xvectors2=$exp2/exp/xvector_${dataset}/xvector.scp
+  mkdir -p $exp_dir/xvectors_${dataset}
+  cp $exp1/exp/xvectors_${dataset}/utt2lang $exp_dir/xvectors_${dataset}
+  xvectors1=$exp1/exp/xvectors_${dataset}/xvector.scp
+  xvectors2=$exp2/exp/xvectors_${dataset}/xvector.scp
+
+  echo "$xvectors1"
+  echo "$xvectors2"
 
   paste-feats \
     ark:"copy-vector scp:$xvectors1 ark,t:- | sed -E 's/\[/\[\n/' | sed -E 's/\]/\n\]/' |" \
     ark:"copy-vector scp:$xvectors2 ark,t:- | sed -E 's/\[/\[\n/' | sed -E 's/\]/\n\]/' |" \
-    ark,scp:$exp_dir/xvector_${dataset}/xvector.ark,$exp_dir/xvector_${dataset}/xvector.scp
+    ark,t:$exp_dir/xvectors_${dataset}/xvector_tmp.ark
+  
+  sed -i ':a;N;$!ba;s/\[\n/\[/g' $exp_dir/xvectors_${dataset}/xvector_tmp.ark
+
+  copy-vector ark,t:$exp_dir/xvectors_${dataset}/xvector_tmp.ark \
+    ark,scp:$exp_dir/xvectors_${dataset}/xvector.ark,$exp_dir/xvectors_${dataset}/xvector.scp
 done
 
 langs=($GP_LANGUAGES)
